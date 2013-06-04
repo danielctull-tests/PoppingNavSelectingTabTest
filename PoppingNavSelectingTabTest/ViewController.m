75566319dda12cdfdc4c1ee0819887f46afa3c84
Daniel Tull <dt@danieltull.co.uk>
2013-06-04 09:02:40 +0100

    Initial Commit

---
+++ PoppingNavSelectingTabTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PoppingNavSelectingTabTest
+//
+//  Created by Daniel Tull on 04.06.2013.
+//  Copyright (c) 2013 Daniel Tull. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ PoppingNavSelectingTabTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PoppingNavSelectingTabTest
+//
+//  Created by Daniel Tull on 04.06.2013.
+//  Copyright (c) 2013 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
